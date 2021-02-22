4bf8331aaeca327a1cd8c915d9db3d7c966f1bbc
ibrahim <deniz351961@gmail.com>
2021-02-22 18:59:43 +0300

    Refactor index.js and update .gitignore

---
+++ startup/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const logger = require("../utility/logger");
+
+module.exports = function () {
+  const db = config.get("db");
+
+  mongoose
+    .connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+      useCreateIndex: true,
+      retryWrites: false,
+    })
+    .then(() => {
+      logger.info("Connected to databese");
+    });
+};
---
+++ startup/routes.js
@@ -0,0 +1,17 @@
+const users = require("../routers/users");
+const auth = require("../routers/auth");
+const library = require("../routers/library");
+const members = require("../routers/members");
+const loans = require("../routers/loans");
+const returns = require("../routers/returns");
+const express = require("express");
+
+module.exports = function (app) {
+  app.use(express.json());
+  app.use("/api/library", library);
+  app.use("/api/members", members);
+  app.use("/api/users", users);
+  app.use("/api/auth", auth);
+  app.use("/api/loans", loans);
+  app.use("/api/returns", returns);
+};
